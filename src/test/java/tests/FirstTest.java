21f81a7e586334aa7d5604f6e38e5b9a42e462ba
AOmar <omar051000@gmail.com>
2022-09-25 15:34:15 +0600

    add text-box test

--- src/test/java/tests/FirstTest.java
+++ src/test/java/tests/FirstTest.java
@@ -1,10 +1,12 @@
 package tests;
 
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -12,10 +14,24 @@ import static com.codeborne.selenide.Selenide.open;
 public class FirstTest {
     @BeforeAll
     static void beforeAll(){
-        open("https://www.google.ru/");
+        Configuration.baseUrl = "https://demoqa.com/";
+        Configuration.holdBrowserOpen = true;
+        Configuration.browserSize = "1920x1080";
     }
     @Test
-    void test(){
-        $(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input")).sendKeys("Ануар Омар");
+    void textBox(){
+        open("text-box");
+        $(By.xpath("//input[@id='userName']")).setValue("Omar Anuar");
+        $(By.xpath("//input[@type='email']")).setValue("omar051000@gmail.com");
+        $(By.xpath("//textarea[@id='currentAddress']")).setValue("Country: Kazakhastan, City: Almaty, street:Abaya-34");
+        $(By.xpath("//textarea[@id='permanentAddress']")).setValue("Country: Kazakhastan, City: Almaty, street:Abaya-35");
+        $(By.xpath("//button[@id='submit']")).click();
+
+        $("#output #name").shouldHave(text("Omar Anuar"));
+        $("#output #email").shouldHave(text("omar051000@gmail.com"));
+        $("#output #currentAddress").shouldHave(text("Country: Kazakhastan, City: Almaty, street:Abaya-34"));
+        $("#output #permanentAddress").shouldHave(text("Country: Kazakhastan, City: Almaty, street:Abaya-35"));
+
+
     }
 }
